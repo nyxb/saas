a137907f66122f605af4536b9d948060a3a62779
nyxb <dennis.ollhoff@joypix.gg>
2024-10-02 18:57:52 +0200

    ✨ feat(layout.tsx): set default theme to dark for better user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
          >
             <ThemeProvider
                attribute="class"
-               defaultTheme="light"
+               defaultTheme="dark"
                enableSystem={false}
             >
                {children}
